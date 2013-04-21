eeca5a659790a6558d489e7ea348b5eb49607dee
MickeyETF <m.milicevic.etf@gmail.com>
2013-04-22 00:21:49 +0300

    Update nepolozeni_ispiti.js

--- nepolozeni_ispiti.js
+++ nepolozeni_ispiti.js
@@ -3,17 +3,23 @@ document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() 
 	{
 		alert("device ready");
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+       Read();
    	 }
 
+    
+     function Read() {
+       alert("Ovde sam");
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+       
+    }
+
+
     function gotFS(fileSystem) {
-    	alert("pristupam fajlu");
         fileSystem.root.getFile("prijava_ispita/nepolozeni_ispiti.json", null, gotFileEntry, fail);
     }
 
     function gotFileEntry(fileEntry) {
         //fileEntry.file(gotFile, fail);
-        alert("Putanja je");
-	   var adresa=fileEntry.fullPath;
-	   alert(adresa);
+        alert("Putanja je")
+       alert(fileEntry.fullPath);
     }

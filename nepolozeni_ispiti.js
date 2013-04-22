0376f2feeba8461e4a97fcfa2a86965d2d8ffebc
MickeyETF <m.milicevic.etf@gmail.com>
2013-04-22 11:46:10 +0200

    male izmejene

--- nepolozeni_ispiti.js
+++ nepolozeni_ispiti.js
@@ -1,27 +1,41 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
+			var pom=0;
+			document.addEventListener("online", onOnline, false);
+			document.addEventListener("deviceready", onDeviceReady, false);
+			
+	function onOnline()
+			{
+			pom=1;
+			}
+			
     function onDeviceReady() 
 	{
-		alert("device ready");
-       Read();
-   	 }
-
-    
-     function Read() {
-       alert("Ovde sam");
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-       
     }
-
-
+	
     function gotFS(fileSystem) {
-    	alert("grr sam");
         fileSystem.root.getFile("prijava_ispita/nepolozeni_ispiti.json", null, gotFileEntry, fail);
     }
 
     function gotFileEntry(fileEntry) {
-        //fileEntry.file(gotFile, fail);
-        alert("Putanja je");
-        var adresa=fileEntry.fullPath;
-       alert(adresa);
+        fileEntry.file(gotFile, fail);
+    }
+
+
+    function gotFile(file)
+    {
+        readAsText(file);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function(evt) {
+           alert("Read as text");
+           var nepolozeni_ispiti=evt.target.result;
+            alert(nepolozeni_ispiti);
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(evt) {
+        alert(evt.target.error.code);
     }
